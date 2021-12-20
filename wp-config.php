a0a931983027c8048fef726519f78874b6c83782
Terry Lorber <terry@metatooth.com>
2021-12-20 08:06:10 -0500

    paranthesize

--- wp-config.php
+++ wp-config.php
@@ -105,7 +105,7 @@ if ( isset($_SERVER['HTTP_X_FORWARDED_HOST']) ) {
         $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
 }
 
-$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+$s = empty($_SERVER["HTTPS"]) ? '' : (($_SERVER["HTTPS"] == "on") ? "s" : "");
 $sp = strtolower($_SERVER["SERVER_PROTOCOL"]);
 $protocol = substr($sp, 0, strpos($sp, "/")) . $s;
 $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
