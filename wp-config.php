d18047299c51c95d3874c86ae30adfbeae479f46
Terry Lorber <terry@metatooth.com>
2021-12-29 09:37:08 -0500

    content & wp

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
  */
 
  // Load Composer’s autoloader
-require_once (__DIR__.'/vendor/autoload.php');
+require_once (__DIR__.'/content/vendor/autoload.php');
 
 // Move the location of the content dir
-define('WP_CONTENT_DIR', dirname(__FILE__).'/wp/wp-content');
+define('WP_CONTENT_DIR', dirname(__FILE__).'/content');
 
 define( 'AS3CF_SETTINGS', serialize( array(
     'provider' => 'aws',
